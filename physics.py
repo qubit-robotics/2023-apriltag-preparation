96228af1add971e632370e9738f9b9c7372a203f
BerkeSinanYetkin <berkesinanyetkin@gmail.com>
2022-12-24 14:45:27 +0300

    Template for our apriltag experiments

---
+++ physics.py
@@ -0,0 +1,45 @@
+#
+# See the documentation for more details on how this works
+#
+# The idea here is you provide a simulation object that overrides specific
+# pieces of WPILib, and modifies motors/sensors accordingly depending on the
+# state of the simulation. An example of this would be measuring a motor
+# moving for a set period of time, and then changing a limit switch to turn
+# on after that period of time. This can help you do more complex simulations
+# of your robot code without too much extra effort.
+#
+
+import wpilib.simulation
+
+from pyfrc.physics.core import PhysicsInterface
+from pyfrc.physics import motor_cfgs, tankmodel
+from pyfrc.physics.units import units
+
+import typing
+
+if typing.TYPE_CHECKING:
+    from robot import MyRobot
+
+
+class PhysicsEngine:
+    """
+    Simulates a motor moving something that strikes two limit switches,
+    one on each end of the track. Obviously, this is not particularly
+    realistic, but it's good enough to illustrate the point
+    """
+
+    def __init__(self, physics_controller: PhysicsInterface, robot: "MyRobot"):
+
+        self.physics_controller = physics_controller
+
+    def update_sim(self, now: float, tm_diff: float) -> None:
+        """
+        Called when the simulation parameters for the program need to be
+        updated.
+
+        :param now: The current time as a float
+        :param tm_diff: The amount of time that has passed since the last
+                        time that this function was called
+        """
+
+        pass
